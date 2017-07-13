b268f52d28acb40fa1d9c2793ee94c3952b04935
Thomas <tmgiewont@gmail.com>
2017-07-13 15:24:22 -0400

    Update to followers input

--- Followers.py
+++ Followers.py
@@ -1,13 +1,16 @@
 import tweepy
 import time
 import random
+import sys
 
 auth = tweepy.OAuthHandler("YJ5mxyUgJLmedR557EoeM3c1m","qZjccKWikCwQgcCoGqgcYvj0Pc3ai5ykLQVj19Q7ybFPU65Sgj")
 auth.set_access_token("753680282977525760-UGdZfSS4YdxEeH8bYc7PwLi6uMIQmuk","VutjO8k5QITPjvKtowKVr12LXrsUKsuMArVmSlXiIv0wP")
 
 api = tweepy.API(auth)
 
-accountvar = raw_input("Account name: ")
+accountvar = sys.argv[1]
+
+# accountvar = raw_input("Account name: ")
 
 users = tweepy.Cursor(api.followers, screen_name=accountvar).items()
 
