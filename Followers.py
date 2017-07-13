2d09829646d6851a1ef8e27ede9d23c489b4207f
Thomas <tmgiewont@gmail.com>
2017-07-13 15:53:49 -0400

    Added logging

--- Followers.py
+++ Followers.py
@@ -2,6 +2,7 @@ import tweepy
 import time
 import random
 import sys
+import logging
 
 auth = tweepy.OAuthHandler("YJ5mxyUgJLmedR557EoeM3c1m","qZjccKWikCwQgcCoGqgcYvj0Pc3ai5ykLQVj19Q7ybFPU65Sgj")
 auth.set_access_token("753680282977525760-UGdZfSS4YdxEeH8bYc7PwLi6uMIQmuk","VutjO8k5QITPjvKtowKVr12LXrsUKsuMArVmSlXiIv0wP")
@@ -13,7 +14,7 @@ accountvar = sys.argv[1]
 # accountvar = raw_input("Account name: ")
 
 users = tweepy.Cursor(api.followers, screen_name=accountvar).items()
-
+logging.basicConfig(filename='Followers.log',level=logging.DEBUG)
 i = 0
 while True:
    try:
@@ -21,7 +22,8 @@ while True:
        user = next(users)
        user = next(users)
        api.create_friendship(user.screen_name)
-       print i
+       print i , user.screen_name
+       logging.info('%s, %s', i, user.screen_name)
        time.sleep(60 + random.randint(0,60))
    except:
        time.sleep(60*15)
