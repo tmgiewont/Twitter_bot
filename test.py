6166e7b47ad4846fadbaf32605522b2b9f2b50e3
tmgiewont <tmgiewont@gmail.com>
2020-09-15 13:07:22 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import tweepy
 
-auth = tweepy.OAuthHandler("YJ5mxyUgJLmedR557EoeM3c1m","qZjccKWikCwQgcCoGqgcYvj0Pc3ai5ykLQVj19Q7ybFPU65Sgj") #consumer key,consumer secret
-auth.set_access_token("753680282977525760-UGdZfSS4YdxEeH8bYc7PwLi6uMIQmuk","VutjO8k5QITPjvKtowKVr12LXrsUKsuMArVmSlXiIv0wP") #acess token,acess token secret
+auth = tweepy.OAuthHandler("") #consumer key,consumer secret
+auth.set_access_token("") #acess token,acess token secret
 
 api = tweepy.API(auth)
 
