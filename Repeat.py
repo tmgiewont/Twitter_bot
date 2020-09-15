2c5520e0b511c720400682585ddb22fee1c2dc2b
tmgiewont <tmgiewont@gmail.com>
2020-09-15 13:09:50 -0400

    Update Repeat.py

--- Repeat.py
+++ Repeat.py
@@ -3,8 +3,8 @@ import time
 import numpy as np
 import random
 
-auth = tweepy.OAuthHandler("YJ5mxyUgJLmedR557EoeM3c1m","qZjccKWikCwQgcCoGqgcYvj0Pc3ai5ykLQVj19Q7ybFPU65Sgj")
-auth.set_access_token("753680282977525760-UGdZfSS4YdxEeH8bYc7PwLi6uMIQmuk","VutjO8k5QITPjvKtowKVr12LXrsUKsuMArVmSlXiIv0wP")
+auth = tweepy.OAuthHandler("","")
+auth.set_access_token("","")
 
 api = tweepy.API(auth)
 Tweet_list = ["Trump is the man who will deal with ISIS",
